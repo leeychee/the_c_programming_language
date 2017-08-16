c3d0f855ed9e84125d9bda14ad2e8c2befcd52cc
Chunhui Liu <leeychee@gmail.com>
2017-08-16 23:04:39 +0800

    hello world, f2c

---
+++ fahr_to_celsius/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    float fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    printf("%3s\t%6s\n", "F", "C");
+    printf("---------------\n");
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = (5.0 / 9.0) * (fahr - 32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
